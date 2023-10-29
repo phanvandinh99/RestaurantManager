16cf369de88fcaa0dbe6f49b23a9aafc0e17b53e
Phan Định <vonhanh271@gmail.com>
2023-10-29 21:59:43 +0700

    Update Sourcecode

---
+++ DoAn/QuanLyNhaHang/Areas/KhachHang/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using QuanLyNhaHang.Models;
+using System.Web.Mvc;
+
+namespace QuanLyNhaHang.Areas.KhachHang.Controllers
+{
+    public class HomeController : Controller
+    {
+        DatabaseQuanLyNhaHang db = new DatabaseQuanLyNhaHang();
+
+        // Hiển thị trang danh sách khách hàng
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
