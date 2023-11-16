e67cea07092b6a9b7ebfec6147ec2f9ff636ec6f
Phan Định <vonhanh271@gmail.com>
2023-11-16 23:45:40 +0700

    Update LienHe and SuKien

---
+++ DoAn/QuanLyNhaHang/Areas/KhachHang/Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyNhaHang.Areas.KhachHang.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: KhachHang/About
+        public ActionResult SuKien()
+        {
+            return View();
+        }
+
+        public ActionResult LienHe()
+        {
+            return View();
+        }
+    }
+}
