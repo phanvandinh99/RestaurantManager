d03e743fbc2e4be4b0d05d23d64393bf4fc0f6f2
Phan Định <vonhanh271@gmail.com>
2023-12-12 23:53:31 +0700

    update chat witai

---
+++ DoAn/QuanLyNhaHang/Common/WitAiService.cs
@@ -0,0 +1,30 @@
+﻿using QuanLyNhaHang.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class WitAiService
+{
+    private readonly string witApiKey;
+    private readonly HttpClient httpClient;
+
+    public WitAiService(string witApiKey)
+    {
+        this.witApiKey = witApiKey;
+        this.httpClient = new HttpClient();
+    }
+
+    public async Task<WitAiResponse> ProcessMessage(string message)
+    {
+        var apiUrl = "https://api.wit.ai/message?v=20231210&q=" + Uri.EscapeDataString(message);
+
+        httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {witApiKey}");
+
+        var response = await httpClient.GetStringAsync(apiUrl);
+
+        // Deserialize the response JSON as needed
+        // Example: var result = JsonConvert.DeserializeObject<WitAiResponse>(response);
+
+        return new WitAiResponse { Text = response };
+    }
+}
