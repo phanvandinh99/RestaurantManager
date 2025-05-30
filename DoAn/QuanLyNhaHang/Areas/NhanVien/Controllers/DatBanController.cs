227eafd4ae8ac8a32a1657621c1defca724c4e1e
Phan Dinh <81685801+phanvandinh99@users.noreply.github.com>
2025-05-30 23:37:46 +0700

    Create DatBan with Employee

---
+++ DoAn/QuanLyNhaHang/Areas/NhanVien/Controllers/DatBanController.cs
@@ -0,0 +1,23 @@
+﻿using QuanLyNhaHang.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace QuanLyNhaHang.Areas.NhanVien.Controllers
+{
+    public class DatBanController : Controller
+    {
+        DatabaseQuanLyNhaHang db = new DatabaseQuanLyNhaHang();
+
+        // Danh sách đặt bàn khách hàng
+        public ActionResult Index(int? iTrangThai)
+        {
+            var listDatBan = db.DatBan
+                .Where(n => iTrangThai == 2 ? n.TrangThai == 2 : n.TrangThai != 2)
+                .OrderBy(n => n.ThoiGianBatDau)
+                .ToList();
+
+            return View(listDatBan);
+        }
+
+    }
+}
